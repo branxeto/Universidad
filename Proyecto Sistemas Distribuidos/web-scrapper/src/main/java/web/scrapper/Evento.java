3fa48adb24b8686991456ab032d17c04766c76f5
Branco Agustin Burotto Vasquez <128112488+branxeto@users.noreply.github.com>
2025-04-25 18:35:30 -0400

    Add files via upload

---
+++ Proyecto Sistemas Distribuidos/web-scrapper/src/main/java/web/scrapper/Evento.java
@@ -0,0 +1,60 @@
+package web.scrapper;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.JsonNode;
+
+@JsonIgnoreProperties({"jams"})
+public class Evento {
+    private long endTimeMillis;
+    private long startTimeMillis;
+    private String startTime;
+    private String endTime;
+    //private JsonNode jams;
+    private JsonNode alerts;
+
+    public Evento() {}
+
+    // <--- GETTERS --->
+    public long getEndTimeMillis() {
+        return this.endTimeMillis;
+    }
+    public long getStartTimeMillis() {
+        return this.startTimeMillis;
+    }
+    public String getStartTime() {
+        return this.startTime;
+    }
+    public String getEndTime() {
+        return this.endTime;
+    }
+    /*
+    public JsonNode getJams() {
+        return this.jams;
+    }
+    */
+    public JsonNode getAlerts() {
+        return this.alerts;
+    }
+
+
+    // <--- SETTERS --->
+    public void setEndTimeMillis(long endTimeMillis) {
+        this.endTimeMillis = endTimeMillis;
+    }
+    public void setStartTimeMillis(long startTimeMillis) {
+        this.startTimeMillis = startTimeMillis;
+    }
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+    /*
+    public void setJams(JsonNode jams) {
+        this.jams = jams;
+    } 
+    */
+    public void setAlerts(JsonNode alerts) {
+        this.alerts = alerts;
+    }    
+}
---
+++ Proyecto Sistemas Distribuidos/web-scrapper/src/main/java/web/scrapper/WebScrapper.java
@@ -0,0 +1,84 @@
+package web.scrapper;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.bson.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.InsertOneResult;
+
+public class WebScrapper {
+    private static final Logger logger = LoggerFactory.getLogger(WebScrapper.class);
+    public static void main(String[] args) throws Exception {
+        
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+        scheduler.scheduleAtFixedRate(() -> {
+            try {
+                ejecutarTarea();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, 0, 1, TimeUnit.MINUTES);
+
+    }
+
+    public static void ejecutarTarea() throws Exception {
+        String json = ""; //Aqui se va a guardar el JSON crudo
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // <--- CONSULTA HTTP --->
+        try {
+            //Construir Request
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("https://www.waze.com/live-map/api/georss?top=-32.9167&bottom=-34.3167&left=-71.7167&right=-69.7833&env=row&types=alerts,traffic"))
+                .GET()
+                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36 OPR/117.0.0.0")
+                .build();
+
+            // Enviar y recibir respuesta que el cliente mando (Request)
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                .send(request, HttpResponse.BodyHandlers.ofString());
+            json = response.body(); 
+            //System.out.println(json);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // <--- MANEJAR FORMATO JSON --->
+        Evento evento = objectMapper.readValue(json, Evento.class);
+        String eventoString = objectMapper.writeValueAsString(evento);
+        System.out.println("Evento: " + eventoString);
+
+        // <--- Base de Datos --->
+        String uri = "mongodb+srv://Branco:Branco1323@universidad.wavnkf6.mongodb.net/?retryWrites=true&w=majority&appName=Universidad";
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("Sistemas_Distribuidos");
+            MongoCollection<Document> collection = database.getCollection("Datos");
+            Document doc = Document.parse(eventoString); 
+            if (doc != null) {
+                InsertOneResult result = collection.insertOne(doc);
+                logger.info("Documento insertado con ID: " + result.getInsertedId());
+            } else {
+                System.out.println("Hubo un error en la obtención del JSON.");
+            }
+        }
+    }
+}
